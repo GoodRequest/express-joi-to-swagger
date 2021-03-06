2694be388a5e42b91dc85932b1b691dd4db0e0f0
Lubomir Igonda <lubomir.igonda@goodrequest.com>
2021-03-06 11:40:48 +0100

    git cache cleared

--- src/ui/src.js
+++ src/ui/src.js
@@ -3,7 +3,15 @@ import 'swagger-ui/dist/swagger-ui.css'
 
 import spec from './data.json'
 
+const CaseInsensitiveFilterPlugin = () => ({
+	fn: {
+		opsFilter: (taggedOps, phrase) => taggedOps.filter((tagObj, tag) => tag.toLowerCase().indexOf(phrase.toLowerCase()) !== -1)
+	}
+})
+
 SwaggerUI({
 	spec,
-	dom_id: '#swagger'
+	dom_id: '#swagger',
+	filter: true,
+	plugins: [CaseInsensitiveFilterPlugin]
 })
