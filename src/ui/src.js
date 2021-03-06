a1857f41100626e4a137a37a7987dea4264baf4d
Lubomir Igonda <lubomir.igonda@goodrequest.com>
2021-03-06 11:49:05 +0100

    add experimental search plugin

--- src/ui/src.js
+++ src/ui/src.js
@@ -9,6 +9,39 @@ const CaseInsensitiveFilterPlugin = () => ({
 	}
 })
 
+/* eslint-disable */
+const AdvancedFilterPlugin = (system) => ({
+	fn: {
+		opsFilter: (taggedOps, phrase) => {
+			phrase = phrase.toLowerCase()
+			const normalTaggedOps = JSON.parse(JSON.stringify(taggedOps))
+			for (tagObj in normalTaggedOps) {
+				const operations = normalTaggedOps[tagObj].operations
+				const i = operations.length;
+				while (i--) {
+					const operation = operations[i].operation
+				if ((operations[i].path.toLowerCase().indexOf(phrase) === -1)
+					&& (operation.summary.toLowerCase().indexOf(phrase) === -1)
+					&& (operation.description.toLowerCase().indexOf(phrase) === -1)
+				) {
+					operations.splice(i, 1)
+				}
+				}
+				if (operations.length == 0 ) {
+					delete normalTaggedOps[tagObj]
+				}
+				else {
+					normalTaggedOps[tagObj].operations = operations;
+				}
+			}
+
+			return system.Im.fromJS(normalTaggedOps);
+			}
+		}
+	})
+
+/* eslint-enable */
+
 SwaggerUI({
 	spec,
 	dom_id: '#swagger',
